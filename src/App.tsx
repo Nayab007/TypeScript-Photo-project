fdb45f9630ed36cd4f3b6edca4adb1475ab883a6
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2022-04-05 08:39:36 +0200

    add

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,7 @@ export default function App() {
         </button>
         Page {page}
         <button id="nextButton" onClick={changePage}>
-          
+          N
         </button>
       </div>
       </div>
