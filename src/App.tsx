e0a1d06237c97d594d530f8f2e298a0a8e10dde8
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2022-04-14 09:22:34 +0200

    add

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ export default function App() {
           <input
             className="search-text"
             type="text"
-            placeholder="Search Author name"
+            placeholder="Search Author namee"
           >
             
           
