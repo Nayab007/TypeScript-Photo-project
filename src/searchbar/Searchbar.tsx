8ab88d8fdc8616f1bbf184dff59b790954dad9a7
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2022-05-09 00:47:18 +0200

    add

--- src/searchbar/Searchbar.tsx
+++ src/searchbar/Searchbar.tsx
@@ -1,39 +1,45 @@
 import { useEffect, useState } from "react";
-
-import "./Searchbar.css";
-
+import * as ReactDOM from "react-dom";
 import axios from "axios";
 
+function App() {
+  const [query, setQuery] = useState("");
+  const [data, setData] = useState([]);
 
-function Searchbar() {
-    const [query, setQuery] = useState("");
-    const [data, setData] = useState([]);
-    
-    // Add one more state to store the authors being searched for
-    const [searchResults, setSearchResults] = useState([]);
-    
-    useEffect(() => {
-            const fetchData = async () => {
-                const res = await axios.get(`https://picsum.photos/v2/list`);
-                setData(res.data);
-            };
-            fetchData();
-        }, []);
-    
-    // Query that handles searching
-    useEffect(() => {
-      setSearchResults(data.filter((authorData) => authorData['author'].toLowerCase().includes(query)));
-    }, [query, data]);
-    
-        return (
-            <div className="app">
-                <input
-                    className="search"
-                    placeholder="Search..."
-                    onChange={(e) => setQuery(e.target.value.toLowerCase())}
-                />
-                <div>{searchResults}</div>
-            </div>
-        );
+  // Add one more state to store the authors being searched for
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get(`https://picsum.photos/v2/list`);
+      setData(res.data);
+    };
+    fetchData();
+  }, []);
+
+  // Query that handles searching
+  useEffect(() => {
+    setSearchResults(
+      data.filter((authorData) =>
+        authorData["author"].toLowerCase().includes(query)
+      )
+    );
+  }, [query, data]);
+
+  return (
+    <div className="app">
+      <input
+        className="search"
+        placeholder="Search..."
+        onChange={(e) => setQuery(e.target.value.toLowerCase())}
+      />
+      <div>
+        {searchResults.map((author) => (
+          <div>{author.author}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
-    export default Searchbar;
+
+ReactDOM.render(<App />, document.getElementById("root"));
