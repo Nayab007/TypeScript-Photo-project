6fc02f6a608881e987880110787e4d9174ca0046
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2022-05-09 00:43:57 +0200

    add

--- src/searchbar/Searchbar.tsx
+++ src/searchbar/Searchbar.tsx
@@ -5,30 +5,35 @@ import "./Searchbar.css";
 import axios from "axios";
 
 
-
 function Searchbar() {
     const [query, setQuery] = useState("");
     const [data, setData] = useState([]);
-  
+    
+    // Add one more state to store the authors being searched for
+    const [searchResults, setSearchResults] = useState([]);
+    
     useEffect(() => {
-      const fetchData = async () => {
-        const res = await axios.get(`https://picsum.photos/v2/?=${query}`);
-        setData(res.data);
-      };
-      if (query.length === 0 || query.length > 2) fetchData();
-    }, [query]);
-  
-    return (
-      <div className="app">
-          <input
-            className="search"
-            placeholder="Search..."
-            onChange={(e) => setQuery(e.target.value.toLowerCase())}
-          />
-        <div>{data}</div>
-      </div>
-    );
-  }
-  
-  export default Searchbar;
-  
+            const fetchData = async () => {
+                const res = await axios.get(`https://picsum.photos/v2/list`);
+                setData(res.data);
+            };
+            fetchData();
+        }, []);
+    
+    // Query that handles searching
+    useEffect(() => {
+      setSearchResults(data.filter((authorData) => authorData['author'].toLowerCase().includes(query)));
+    }, [query, data]);
+    
+        return (
+            <div className="app">
+                <input
+                    className="search"
+                    placeholder="Search..."
+                    onChange={(e) => setQuery(e.target.value.toLowerCase())}
+                />
+                <div>{searchResults}</div>
+            </div>
+        );
+}
+    export default Searchbar;
